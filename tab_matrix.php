ec7a9a629b6443399096d327f3b0e76a906b5f90
jpb <jean-pascal.boudet@atm-consulting.fr>
2020-08-20 17:32:27 +0200

    refactoring and clean code

--- tab_matrix.php
+++ tab_matrix.php
@@ -22,34 +22,8 @@
  *    \brief      Page to edit matrix qts on active object [ invoice / order / propal ]
  */
 
+require_once ('config.dolibarr.inc.php');
 
-// Load Dolibarr environment
-$res = 0;
-// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"]))
-	$res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php";
-// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
-$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
-$tmp2 = realpath(__FILE__);
-$i = strlen($tmp) - 1;
-$j = strlen($tmp2) - 1;
-while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
-	$i--;
-	$j--;
-}
-if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/main.inc.php"))
-	$res = @include substr($tmp, 0, ($i + 1)) . "/main.inc.php";
-if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php"))
-	$res = @include dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php";
-// Try main.inc.php using relative path
-if (!$res && file_exists("../main.inc.php"))
-	$res = @include "../main.inc.php";
-if (!$res && file_exists("../../main.inc.php"))
-	$res = @include "../../main.inc.php";
-if (!$res && file_exists("../../../main.inc.php"))
-	$res = @include "../../../main.inc.php";
-if (!$res)
-	die("Include of main fails");
 
 dol_include_once('/linesfromproductmatrix/class/bloc.class.php');
 dol_include_once('/linesfromproductmatrix/lib/linesfromproductmatrix_bloc.lib.php');
@@ -204,12 +178,11 @@ if ($id > 0 || !empty($ref)) {
 
 
 $TlinesObectj = array();
-     //  var_dump($object);
-	// var_dump($object->element);
+
  foreach ($object->lines as $l ){
  	$obj = new stdclass();
-	 $TlinesObectj[$l->fk_product] =
- 	var_dump("Qty : ".$l->qty . "  -- ".$l->product_label . '--'.$l->product_ref . "-- fk_product : ".$l->fk_product);
+	// $TlinesObectj[$l->fk_product] =
+ 	//var_dump("Qty : ".$l->qty . "  -- ".$l->product_label . '--'.$l->product_ref . "-- fk_product : ".$l->fk_product);
  }
 
 	$bloc = new Bloc($db);
